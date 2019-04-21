da0f503282ce41c2be62ee38ce09130879667a90
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2019-04-21 10:20:50 +0100

    base setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="app">Small beginnings</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'))
