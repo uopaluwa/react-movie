cc0a1a379f2c6f02434a3a22a40ae5690495f6b7
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2019-04-26 14:09:20 +0100

    add empty navbar and footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div id="footer">
+        Footer
+      </div>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div id="navbar">
+        Navbar
+      </div>
+    )
+  }
+}
+
+export default NavBar;
