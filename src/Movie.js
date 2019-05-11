9a0acd30316c41deeddee0df8b4bf271a75bfd3a
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2019-05-11 13:34:29 +0100

    Shorten displayed movie title

--- src/Movie.js
+++ src/Movie.js
@@ -4,6 +4,9 @@ const image_size = 'w200';
 const poster_base_url = `https://image.tmdb.org/t/p/${image_size}`;
 
 class Movie extends React.Component {
+  shortenMovieTitleString(title) {
+    return title.length < 25 ? title : title.substring(0, 25) + '...' 
+  }
   render() {
     const { movie } = this.props;
     return (
@@ -11,7 +14,7 @@ class Movie extends React.Component {
         <div className="poster">
           <img src={poster_base_url + movie.poster_path} alt="movie poster"/>
         </div>
-        <p>{movie.title}</p>
+        <p>{this.shortenMovieTitleString(movie.title)}</p>
         <span>{movie.release_date.split('-')[0]}</span>
       </div>
     )
