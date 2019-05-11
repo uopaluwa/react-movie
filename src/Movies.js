56adbc294ee84d9f3debf94873660d5cc8d38a1e
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2019-05-11 13:37:14 +0100

    Handle movie search

--- src/Movies.js
+++ src/Movies.js
@@ -6,13 +6,48 @@ import { Link } from '@reach/router';
 class Movies extends React.Component {
   constructor(props) {
     super(props)
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       movies: [],
+      search_string: "",
       error: null
     }
   }
   componentDidMount() {
+    this.getPopularMovies();
+  }
+  handleChange(event) {
+    this.setState({
+      search_string: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const search_string = this.state.search_string
+    search_string ? this.searchForMovie(search_string) : this.getPopularMovies()
+  }
+  searchForMovie(search_string) {
+    axios.get('search/movie', {
+      params: {
+        query: search_string
+      }
+    })
+    .then(response => {
+      if (response.data && response.data.results) {
+        this.setState({
+          movies: response.data.results,
+          error: null
+        })
+      }
+    })
+    .catch(error => {
+      this.setState({
+        error: 'An error occurred while retrieving data.'
+      })
+    })
+  }
+  getPopularMovies() {
     axios.get('discover/movie', {
       sort_by: 'popularity_desc'
     })
@@ -32,14 +67,23 @@ class Movies extends React.Component {
   }
   render() {
     return (
-      <div id="movies">
-        {this.state.movies.map(movie => 
-        (
-          <Link to={`/${movie.id}`} key={movie.id}>
-            <Movie movie={movie} />
-          </Link>
-        )
-      )}
+      <div className="movies-container">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="movie-search">
+            Search<br></br>
+            <input id="movie-search" type="text" placeholder="Enter Movie Title" onChange={this.handleChange} value={this.state.search_string} />
+            <button type="submit">Search</button>
+          </label>
+        </form>
+        <div id="movies">
+          {this.state.movies.map(movie =>
+            (
+              <Link to={`/${movie.id}`} key={movie.id}>
+                <Movie movie={movie} />
+              </Link>
+            )
+          )}
+        </div>
       </div>
     )
   }
