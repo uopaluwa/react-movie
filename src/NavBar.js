f6540b1bb490a1520846b3e3e4d76cca1873f0f1
uopaluwa <ufedojo.opaluwa@gmail.com>
2019-09-26 19:03:41 +0100

    highlight active navlink

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from '@reach/router'
 
-class NavBar extends React.Component {
-  render() {
+const NavBar = () => {
+    const highlightActiveLink = (isActive) => {
+      return {
+        style: {
+          borderBottom: isActive && `2px solid #5d7`
+        }
+      }
+    }
+
     return (
       <div id="navbar">
-        <div><Link to="/" className="nav-link">Logo</Link></div>
+        <div><Link to="/" className="nav-link logo">CINEMINFO</Link></div>
         <div>
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/browse" className="nav-link">Browse Movies</Link>
+          <Link to="/" getProps={({ isCurrent }) => highlightActiveLink(isCurrent)} className="nav-link">Home</Link>
+          <Link to="/browse" getProps={({ isCurrent }) => highlightActiveLink(isCurrent)} className="nav-link">Search Movies</Link>
         </div>
       </div>
     )
-  }
 }
 
 export default NavBar;
